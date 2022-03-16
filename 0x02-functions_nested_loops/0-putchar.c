13f160a9b76c2e012964622ced3c17ff7ff4b29c
Oluwajuwon Odunitan <oduns07@gmail.com>
2022-03-16 13:28:32 +0100

    message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints Putchar
+ * main - prints _putchar
  *
  * Description: prints _putchar
  *
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
 
 	return (0);
