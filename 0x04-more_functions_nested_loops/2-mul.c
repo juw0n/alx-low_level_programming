f0e0ebfa53f3fb60bac6947383a5523f1436bec4
Oluwajuwon Odunitan <oduns07@gmail.com>
2022-03-17 07:17:01 +0100

    message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - is a program that multiplies tow integers.
+ * @a: is firts digit
+ * @b: is second digit
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+		return (a * b);
+}
