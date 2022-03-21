e6e791f8e72b50a02fffd12a5e6e64739bbceac4
Oluwajuwon Odunitan <oduns07@gmail.com>
2022-03-21 09:49:17 +0100

    update main files

--- 0x05-pointers_arrays_strings/main_0.c
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
-
-/**
- * Declare the prototype
- */
-
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
